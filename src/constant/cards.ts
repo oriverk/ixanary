7e7c592e6b62928261790da67ca45a896f65e915
oriverk <oriveryk@gmail.com>
2021-01-26 08:15:04 +0000

    add constant data in src/constant dir

---
+++ src/constant/cards.ts
@@ -0,0 +1,82 @@
+export const cards = [
+  {
+    id: 1001,
+    occupation: '将',
+    rarelity: '天',
+    name: '織田信長',
+    cost: 3.5,
+    capacity: 3500,
+    parameter: { spear: 'a', bow: 'b', horse: 'a', weapon: 's' },
+    offence: 745,
+    defence: 710,
+    tactic: 465,
+    offenceGrowth: 19,
+    deffenceGrowth: 19,
+    tacticGrowth: 2.5,
+    skil: {
+      type: 'offence',
+      name: '三段撃 激烈'
+    },
+    unitSkil:['']
+  },
+  {
+    id: 2001,
+    occupation: '将',
+    rarelity: '極',
+    name: '本多忠勝',
+    cost: 3.0,
+    capacity: 2790,
+    parameter: { spear: 'a', bow: 'b', horse: 's', weapon: 'c' },
+    offence: 745,
+    defence: 725,
+    tactic: 385,
+    offenceGrowth: 18,
+    deffenceGrowth: 17,
+    tacticGrowth: 2.0,
+    skil: {
+      type: 'offence',
+      name: '剛勇無双'
+    },
+    unitSkil: ['葵の勇将', '東西無双']
+  },
+  {
+    id: 3001,
+    occupation: '剣',
+    rarelity: '特',
+    name: '上泉信綱',
+    cost: 2.0,
+    capacity: 820,
+    parameter: { spear: 's', bow: 'c', horse: 'c', weapon: 'f' },
+    offence: 950,
+    defence: 950,
+    tactic: 400,
+    offenceGrowth: 50,
+    deffenceGrowth: 50,
+    tacticGrowth: 1.5,
+    skil: {
+      type: 'offence',
+      name: '新陰流'
+    },
+    unitSkil: ['剣の教え']
+  },
+  {
+    id: 4001,
+    occupation: '将',
+    rarelity: '上',
+    name: '武田晴信',
+    cost: 3.0,
+    capacity: 2370,
+    parameter: { spear: 'b', bow: 'c', horse: 'b', weapon: 'd' },
+    offence: 625,
+    defence: 625,
+    tactic: 425,
+    offenceGrowth: 16,
+    deffenceGrowth: 16,
+    tacticGrowth: 2.0,
+    skil: {
+      type: 'offence',
+      name: '騎馬隊挟撃'
+    },
+    unitSkil: ['']
+  },
+]
---
+++ src/constant/links.ts
@@ -0,0 +1,26 @@
+export const links = [
+  {
+    'title': 'top',
+    'url': '/'
+  },
+  {
+    'title': '武将一覧',
+    'url': '/card'
+  },
+  {
+    'title': '武将詳細',
+    'url': '/card/show'
+  },
+  {
+    'title': 'スキル一覧',
+    'url': '/skil'
+  },
+  {
+    'title': 'スキル詳細',
+    'url': '/skil/show'
+  },
+  {
+    'title': '部隊スキル一覧',
+    'url': '/unit-skil'
+  },
+]
