92a80f7c61656aa1e8914a7af634b88bf26a0310
oriverk <oriveryk@gmail.com>
2021-02-03 13:08:34 +0000

    correct tailwind and postcss setting

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,8 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
 // @ts-ignore
-import devCss from '!raw-loader!../style/global.css'
-// @ts-ignore
 import buildCss from '!raw-loader!../style/bundle.css'
-const cssFile = process.env.NODE_ENV === 'development' ? devCss : buildCss
+const cssFile = buildCss
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
