456cf58164a5292861e1b503e6ede421ed5e96ee
oriverk <oriveryk@gmail.com>
2021-02-03 15:50:06 +0000

    bring sitemap generator from oriverk.dev

---
+++ src/libs/genSitemap.ts
@@ -0,0 +1,31 @@
+import ixanary from '../../ixanary.config'
+
+const fixedPage = [
+  '',
+  'cards',
+  'skills',
+  'unit-skills',
+  'search'
+]
+
+export function generateSitemapXml(): string {  
+  let xml = `<?xml version="1.0" encoding="UTF-8"?>`
+  xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`
+
+  fixedPage.forEach((fixed) => {
+      const path = `${ixanary.baseUrl}/${fixed}`
+      const lastmod = new Date()
+
+      xml += `
+        <url>
+          <loc>${path}</loc>
+          <lastmod>${lastmod.toISOString()}</lastmod>
+          <changefreq>weekly</changefreq>
+        </url>
+      `
+  })
+
+  xml += `</urlset>`
+
+  return xml
+}
---
+++ src/pages/sitemap.xml.tsx
@@ -0,0 +1,23 @@
+import { GetServerSidePropsContext } from 'next'
+
+import { generateSitemapXml } from '../libs/genSitemap'
+
+// reference: https://zenn.dev/catnose99/articles/c441954a987c24
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const { res } = ctx
+  const xml = generateSitemapXml()
+  
+  res.statusCode = 200
+  // 866400 = 24h * 60min * 60second
+  res.setHeader('Cache-Control', 's-maxage=86400, state-while-revalidate')
+  res.setHeader('Content-Type', 'text/xml')
+  res.end(xml)
+
+  return {
+    props: {}
+  }
+}
+
+const Component = () => null
+export default Component
