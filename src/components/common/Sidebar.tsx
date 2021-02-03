62a62a4116eb90129dde09ae0617d34afd7e4ab5
oriverk <oriveryk@gmail.com>
2021-02-03 13:09:53 +0000

    modify to display current page link white in sidebar

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { IconContext } from 'react-icons'
 import { MdDashboard } from 'react-icons/md'
 
@@ -9,8 +10,11 @@ type Props = {
   sidebarProps: Pick<SidebarPropsType, 'isOpen'>
 }
 
-export const Sidebar: React.FC<Props> = ({sidebarProps}) => {
+export const Sidebar: React.FC<Props> = ({ sidebarProps }) => {
   const { isOpen } = sidebarProps
+  const router = useRouter()
+
+  const isCurrentPath = (str: string) => (str === router.asPath)
 
   return (
     <div className={`${isOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in' } fixed z-30 inset-y-0 left-0 w-64 transition duration-300 transform bg-gray-900 overflow-y-auto lg:translate-x-0 lg:static lg:inset-0`}>
@@ -31,7 +35,7 @@ export const Sidebar: React.FC<Props> = ({sidebarProps}) => {
       </Link>
       <nav className='mt-10'>
         <Link href={routes.cards}>
-          <a className='flex items-center mt-4 py-2 px-6 bg-gray-700 bg-opacity-25 text-gray-100'>
+          <a className={`flex items-center mt-4 py-2 px-6 ${isCurrentPath(routes.cards) ? 'bg-gray-700 bg-opacity-25 text-gray-100' : 'text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100' } `}>
             <IconContext.Provider value={{ className: 'h-6 w-6' }}>
               <MdDashboard/>
             </IconContext.Provider>
@@ -41,7 +45,7 @@ export const Sidebar: React.FC<Props> = ({sidebarProps}) => {
           </a>
         </Link>
         <Link href={routes.skills}>
-          <a className='flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100'>
+          <a className={`flex items-center mt-4 py-2 px-6 ${isCurrentPath(routes.skills) ? 'bg-gray-700 bg-opacity-25 text-gray-100' : 'text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100' } `}>
             <IconContext.Provider value={{ className: 'h-6 w-6' }}>
               <MdDashboard/>
             </IconContext.Provider>
@@ -51,7 +55,7 @@ export const Sidebar: React.FC<Props> = ({sidebarProps}) => {
           </a>
         </Link>
         <Link href={routes.unitSkills}>
-          <a className='flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100'>
+          <a className={`flex items-center mt-4 py-2 px-6 ${isCurrentPath(routes.unitSkills) ? 'bg-gray-700 bg-opacity-25 text-gray-100' : 'text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100' } `}>
             <IconContext.Provider value={{ className: 'h-6 w-6' }}>
               <MdDashboard/>
             </IconContext.Provider>
@@ -60,14 +64,6 @@ export const Sidebar: React.FC<Props> = ({sidebarProps}) => {
             </span>
           </a>
         </Link>
-        <a href='#' className='flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100'>
-          <IconContext.Provider value={{ className: 'h-6 w-6' }}>
-            <MdDashboard/>
-          </IconContext.Provider>
-          <span className='mx-3'>
-            Forms
-          </span>
-        </a>
       </nav>
     </div>
   )
