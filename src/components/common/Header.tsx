ac9db9210cb09b02edb99c62d540b026f453066d
oriverk <oriveryk@gmail.com>
2021-02-03 15:48:42 +0000

    add aria-label to button for lighthouse

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -20,7 +20,7 @@ export const Header: React.FC<Props> = ({ sidebarProps }) => {
   return (
     <header className="flex justify-between items-center py-4 px-6 border-b-4 border-indigo-600">
       <div className="flex items-center">
-        <button onClick={() => setSidebarState(true)} className="text-gray-500 lg:hidden">
+        <button onClick={() => setSidebarState(true)} className="text-gray-500 lg:hidden" aria-label='open side navigation drawer'>
         {/* <button onClick={()=>setSidebarOpen(true)} className="text-gray-500 focus:outline-none lg:hidden"> */}
           <IconContext.Provider value={{ className: 'h-6 w-6' }}>
             <GiHamburgerMenu/>
@@ -43,7 +43,7 @@ export const Header: React.FC<Props> = ({ sidebarProps }) => {
       </div>
       <div className="flex items-center">
         <div className="relative">
-          <button onClick={() => toggleTheme(theme)} className='flex mx-4 text-yellow-500 focus:outline-none'>
+          <button onClick={() => toggleTheme(theme)} className='flex mx-4 text-yellow-500 focus:outline-none' aria-label='change site color theme'>
             <IconContext.Provider value={{ className: 'h-6 w-6' }}>
               {theme === 'dark' ? <FaMoon /> : <FaSun />}
             </IconContext.Provider>
