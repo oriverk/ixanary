b0969ea193b1b81942aa3836d8e574c5982a44f4
oriverk <oriveryk@gmail.com>
2021-02-09 14:11:14 +0900

    add puppeteer func to get card data json

---
+++ crawler/getCardData.js
@@ -0,0 +1,71 @@
+const path = require('path')
+const fs = require('fs')
+const target = require(
+  `${path.join(process.cwd(), 'lib/index.js')}`
+)
+const puppeteer = require('puppeteer')
+
+async function getCardData(page, url, tableSelecotr) {
+  let data = []
+  const trSelector = `${tableSelecotr} > tbody > tr`
+
+  await page.goto(url)
+  const trs = await page.$$(trSelector)
+
+  for (const tr of trs) {
+    let datum = []
+    datum = await tr.$$eval('td', tds => {
+      return tds.map((td,i) => td.textContent)
+    })
+
+    const units = datum[16].split('/').map(unit => unit.replace('...', ''))
+    
+    data.push({
+      id: datum[0],
+      rarity: datum[1],
+      jobType: datum[17],
+      illustrator: '',
+      description: '',
+      name: datum[2].replace(/\(.+\)/,''),
+      cost: parseFloat(datum[3]),
+      capacity: parseInt(datum[4], 10),
+      yari: datum[5].replace('.',''),
+      yumi: datum[6].replace('.',''),
+      kiba: datum[7].replace('.',''),
+      heiki: datum[8].replace('.',''),
+      attack: parseInt(datum[9], 10),
+      defence: parseInt(datum[10], 10),
+      intelligence: parseInt(datum[11], 10),
+      attackGrowth: parseInt(datum[12], 10),
+      defenceGrowth: parseInt(datum[13], 10),
+      intGrowth: parseFloat(datum[14]),
+      skill: {
+        name: datum[15].split('：')[1],
+        type: datum[15].split('：')[0],
+        description: ''
+      },
+      unitSkill: units
+    })
+  }
+  return data
+}
+
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  
+  for (const url in target.url.project) {
+    data = await getCardData(page, target.url.project[url], target.tableSelector[url])
+    try {
+      fs.writeFileSync(
+      path.join(process.cwd(), `assets/${url}.json`),
+        JSON.stringify(data, undefined, 2),
+        'utf-8'
+      )
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  await browser.close()
+})()
---
+++ crawler/lib/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+  url: {
+    home: 'http://www.ixawiki.com/index.php',
+    project: {
+      ten: 'http://www.ixawiki.com/index.php?Busho%2F%C9%F0%BE%AD%A5%AB%A1%BC%A5%C9%B0%EC%CD%F7%28%C5%B7%29',
+      kiwame: 'http://www.ixawiki.com/index.php?Busho%2F%C9%F0%BE%AD%A5%AB%A1%BC%A5%C9%B0%EC%CD%F7%28%B6%CB%29',
+      toku: 'http://www.ixawiki.com/index.php?Busho%2F%C9%F0%BE%AD%A5%AB%A1%BC%A5%C9%B0%EC%CD%F7%28%C6%C3%29',
+      jou: 'http://www.ixawiki.com/index.php?Busho%2F%C9%F0%BE%AD%A5%AB%A1%BC%A5%C9%B0%EC%CD%F7%28%BE%E5%29',
+      jo: 'http://www.ixawiki.com/index.php?Busho%2F%C9%F0%BE%AD%A5%AB%A1%BC%A5%C9%B0%EC%CD%F7%28%BD%F8%29',
+      hoka: 'http://www.ixawiki.com/index.php?Busho%2F%C9%F0%BE%AD%A5%AB%A1%BC%A5%C9%B0%EC%CD%F7%28%C2%BE%29'
+    }
+  },
+  tableSelector:{
+    ten: '#table_edit_t_1_Busho_2F_C9_F0_BE_ABD_A5_AB_A1_BC_A5_C9_B0_EC_CD_F7_28_C5_B7_29',
+    kiwame:'#table_edit_t_1_Busho_2F_C9_F0_BE_ABD_A5_AB_A1_BC_A5_C9_B0_EC_CD_F7_28_B6_CB_29',
+    toku: '#table_edit_t_1_Busho_2F_C9_F0_BE_ABD_A5_AB_A1_BC_A5_C9_B0_EC_CD_F7_28_C6_C3_29',
+    jou: '#table_edit_t_1_Busho_2F_C9_F0_BE_ABD_A5_AB_A1_BC_A5_C9_B0_EC_CD_F7_28_BE_E5_29',
+    jo:'#table_edit_t_1_Busho_2F_C9_F0_BE_ABD_A5_AB_A1_BC_A5_C9_B0_EC_CD_F7_28_BD_F8_29',
+    hoka:'#table_edit_t_1_Busho_2F_C9_F0_BE_ABD_A5_AB_A1_BC_A5_C9_B0_EC_CD_F7_28_C2_BE_29',
+  }
+}
