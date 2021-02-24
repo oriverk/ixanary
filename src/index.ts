1173d20fa0935bb11345bdf0758cc9f0ea2a2296
oriverk <oriveryk@gmail.com>
2021-02-25 00:39:04 +0900

    add fastify with typescript for getStated sample

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
+import fastify, { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
+const app: FastifyInstance = fastify({ logger: true })
 
-async function main() {
+app.register(require('fastify-formbody'))
+
+app.get('/', async (_: FastifyRequest, __: FastifyReply) => {
+  return { hello: 'world' }
+})
+
+app.get(`/user`, async (_: FastifyRequest, res: FastifyReply) => {
+  const allUsers = await prisma.user.findMany()
+  res.send(allUsers)
+})
+
+const start = async () => {
+  try {
+    await app.listen(3000)
+  } catch (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+}
+start()
+
+// async function main() {
   // await prisma.user.create({
   //   data: {
   //     name: 'hoge',
@@ -17,24 +40,24 @@ async function main() {
   // })
 
   // const allUsers = await prisma.user.findMany({
-  //   include: {
-  //     posts: true,
-  //     profile: true,
-  //   },
+    // include: {
+    //   posts: true,
+    //   profile: true,
+    // },
   // })
   // console.dir(allUsers, { depth: null })
 
-  const post = await prisma.post.update({
-    where: { id: 2 },
-    data: { published: true },
-  })
-  console.log(post)
-}
+  // const post = await prisma.post.update({
+  //   where: { id: 2 },
+  //   data: { published: true },
+  // })
+  // console.log(post)
+// }
 
-main()
-  .catch(e => {
-    throw e
-  })
-  .finally(async () => {
-    await prisma.$disconnect
-  })
+// main()
+//   .catch(e => {
+//     throw e
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect
+//   })
