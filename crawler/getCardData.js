83b9b5b56f36730caea44c61c1ea1b0ddb5981c7
oriverk <oriveryk@gmail.com>
2021-03-05 01:24:37 +0900

    update ixawiki crawler and the data

--- crawler/getCardData.js
+++ crawler/getCardData.js
@@ -15,19 +15,23 @@ async function getCardData(page, url, tableSelecotr) {
   for (const tr of trs) {
     let datum = []
     datum = await tr.$$eval('td', tds => {
-      return tds.map((td,i) => td.textContent)
+      return tds.map((td) => td.textContent)
     })
 
-    const units = datum[16].split('/').map(unit => unit.replace('...', ''))
-    
+    if (datum[2] === '(旧)雑賀孫市') {
+      continue
+    }
+
+    const units = datum[16].split('/').map(unit => unit.replace(/\.+/, ''))
+
     data.push({
       id: parseInt(datum[0],10),
       rarity: datum[1],
       jobType: datum[17],
-      illustrator: '',
+      illustrator: datum[1] === '天' ? '直良有祐' : '',
       description: '',
-      name: datum[2].replace(/\(.+\)/,''),
-      cost: parseFloat(datum[3]),
+      name: datum[2].replace(/\(.+\)/,''),  // 織田信長(新)の(新)を除く
+      cost: parseInt(datum[3], 10),
       capacity: parseInt(datum[4], 10),
       yari: datum[5].replace('.',''),
       yumi: datum[6].replace('.',''),
@@ -38,13 +42,14 @@ async function getCardData(page, url, tableSelecotr) {
       intelligence: parseInt(datum[11], 10),
       attackGrowth: parseInt(datum[12], 10),
       defenceGrowth: parseInt(datum[13], 10),
-      intGrowth: parseFloat(datum[14]),
+      intGrowth: parseInt(datum[14], 10),
       skill: {
         name: datum[15].split('：')[1],
         type: datum[15].split('：')[0],
-        description: ''
       },
-      unitSkill: units
+      unitSkill: {
+        name: units
+      }
     })
   }
   return data
